0e5abf92397cbd1a3319d43f7a1d9de5e03ad791
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-05-01 18:57:19 -0300

    Database config

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({path: "./config.env"});
-const app = require("./app");
+mongoose.connect(process.env.DATABASE_LOCAL, {useNewUrlParser: true, useUnifiedTopology: true}) 
+  .then((connect)=>{
+    console.log("DB connections succesful");
+});
 
+const app = require("./app");
 const port = process.env.PORT || 3030;
 app.listen(port, ()=>{
   console.log(`Server running on port ${port}`);
